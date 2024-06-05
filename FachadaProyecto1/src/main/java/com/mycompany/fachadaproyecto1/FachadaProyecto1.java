18be215db20b160bd82e8bb19e6c99ff359e73bc
jesus imperial <jeskid99@gmail.com>
2024-06-05 13:01:07 -0700

    creacion proyecto fachada

---
+++ FachadaProyecto1/src/main/java/com/mycompany/fachadaproyecto1/FachadaProyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fachadaproyecto1;
+
+/**
+ *
+ * @author jesus
+ */
+public class FachadaProyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
