002c321346e757dbcb1fc5f9c0d329524cd65663
jesus imperial <jeskid99@gmail.com>
2024-06-04 20:26:31 -0700

    prueba1

--- PresentacionProyecto1/src/main/java/com/mycompany/presentacionproyecto1/PresentacionProyecto1.java
+++ PresentacionProyecto1/src/main/java/com/mycompany/presentacionproyecto1/PresentacionProyecto1.java
@@ -12,5 +12,7 @@ public class PresentacionProyecto1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hola");
+        
     }
 }
