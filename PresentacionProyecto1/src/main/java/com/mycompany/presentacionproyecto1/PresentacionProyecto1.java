5999ada6b0e89a3e42c4fdd2e7afc089bf8056d4
jesus imperial <jeskid99@gmail.com>
2024-06-04 18:59:47 -0700

    Initial commit

---
+++ PresentacionProyecto1/src/main/java/com/mycompany/presentacionproyecto1/PresentacionProyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentacionproyecto1;
+
+/**
+ *
+ * @author jesus
+ */
+public class PresentacionProyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ persistenciaProyecto1/src/main/java/com/mycompany/persistenciaproyecto1/PersistenciaProyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistenciaproyecto1;
+
+/**
+ *
+ * @author jesus
+ */
+public class PersistenciaProyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
