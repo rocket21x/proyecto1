de0f31c4df3dea21a094273d27830ad3bee020c4
jesus imperial <jeskid99@gmail.com>
2024-06-08 16:46:46 -0700

    .

--- FachadaAgregar/src/main/java/com/mycompany/fachadaagregar/FachadaAgregar.java
+++ FachadaAgregar/src/main/java/com/mycompany/fachadaagregar/FachadaAgregar.java
@@ -11,6 +11,6 @@ package com.mycompany.fachadaagregar;
 public class FachadaAgregar {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
