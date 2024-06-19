973eb0576a457c0b4295cf145fea5a5273286c19
Mostaza5330 <128562434+Mostaza5330@users.noreply.github.com>
2024-06-19 10:21:23 -0700

    Presentación de pago
    Se inicio la rama de pago

---
+++ PresentacionPago/src/main/java/presentacion/PresentacionPago.java
@@ -0,0 +1,14 @@
+package presentacion;
+
+/**
+ *
+ * @author osval
+ */
+public class PresentacionPago {
+
+    public static void main(String[] args) {
+        
+        new Pago().setVisible(true);
+
+    }
+}
